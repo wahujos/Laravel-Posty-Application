12a0fdd3b6bcebc6ccf09f2ae19f0ec7e844b3c2
wahujos <jwahu30@gmail.com>
2021-04-08 13:04:52 +0300

    initial commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Post;
+use App\Policies\PostPolicy;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        // 'App\Models\Post' => 'App\Policies\PostPolicy',
+        Post::class=>PostPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostLikeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/posts', function () {
+//     return view('posts.index');
+// });
+
+Route::get('/dashboard',[DashboardController::class,'index'])
+->name('dashboard');
+// ->middleware('auth');
+
+Route::get('/register',[RegisterController::class,'index'])->name('register');
+
+Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/login',[LoginController::class,'index'])->name('login');
+
+Route::post('/login',[LoginController::class,'store']);
+
+Route::post('/logout',[LogoutController::class,'store'])->name('logout');
+
+Route::get('/',function(){
+    return view('home');
+})->name('home');
+
+Route::get('/posts',[PostController::class,'index'])->name('posts');
+Route::post('/posts',[PostController::class,'store']);
+Route::delete('/posts/{post}',[PostController::class,'destroy'])->name('posts.destroy');
+
+Route::post('/posts/{post:id}/likes',[PostLikeController::class,'store'])->name('posts.likes');
+Route::delete('/posts/{post:id}/likes',[PostLikeController::class,'destroy'])->name('posts.likes');
+
